9e6367465ca29fb8b370cb1b4f79c75bdcc99dc5
Sagas29 <guada6190@gmail.com>
2018-03-07 15:58:12 -0600

    Subida de archivos

---
+++ Jk/build/CMakeFiles/bns.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bns.dir/bones.cpp.o"
+  "bns.pdb"
+  "bns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
